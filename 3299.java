7d425a93d772ad6a222eb48c85de028e2a90268b
Farhan Sadeek <85633011+FarhanSadeek21@users.noreply.github.com>
2021-12-03 08:26:19 -0500

    Create 3299.java

---
+++ 3299.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner (System.in);
+	    String number = input.nextLine();
+	    if(number.contains("13")){
+	        System.out.printf("%s es de Mala Suerte", number);
+	    }
+	    else {
+	        System.out.printf("%s NO es de Mala Suerte", number);
+	    }
+	    /*
+	    Alternative way to solve the same problem
+	    
+	    if(number.indexOf("13") >= 0){
+	        System.out.printf("%s es de Mala Suerte", number);
+	    }
+	    else {
+	        System.out.printf("%s NO es de Mala Suerte", number);
+	    }
+	    */
+	}
+}
