f3660b153dcbf8ff1dcd452d862e08b8f0b29117
Farhan Sadeek <85633011+FarhanSadeek21@users.noreply.github.com>
2021-12-03 08:43:28 -0500

    Update 3299.java

--- 3299.java
+++ 3299.java
@@ -5,10 +5,10 @@ public class Main
 	    Scanner input = new Scanner (System.in);
 	    String number = input.nextLine();
 	    if(number.contains("13")){
-	        System.out.printf("%s es de Mala Suerte", number);
+	        System.out.printf("%s es de Mala Suerte\n", number);
 	    }
 	    else {
-	        System.out.printf("%s NO es de Mala Suerte", number);
+	        System.out.printf("%s NO es de Mala Suerte\n", number);
 	    }
 	    /*
 	    Alternative way to solve the same problem
